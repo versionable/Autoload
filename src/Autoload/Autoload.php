8c7f54e4ef53fb948736de48526fb1c9c2fe60c5
Harry Walter <harry@versionable.co.uk>
2010-10-26 20:15:19 +0100

    Fixing checkstyle

--- src/Autoload/Autoload.php
+++ src/Autoload/Autoload.php
@@ -69,170 +69,154 @@ namespace Autoload;
  * @copyright Copyright (c) 2010-2015 Versionable (http://www.versionable.co.uk)
  * @license   http://versionable.co.uk/license.html New BSD License
  */
-class Autoload
-{
-  /**
-   *
-   * @var string
-   */
-  private $namespaceSeparator = '\\';
-  /**
-   *
-   * @var string
-   */
-  private $fileExtension = '.php';
-  /**
-   *
-   * @var array
-   */
-  private $namespaces = array();
+class Autoload {
+    /**
+     *
+     * @var string
+     */
+    private $namespaceSeparator = '\\';
+    /**
+     *
+     * @var string
+     */
+    private $fileExtension = '.php';
+    /**
+     *
+     * @var array
+     */
+    private $namespaces = array();
 
-  /**
-   * Creates a new Autoload instance that loads classes of the
-   * specified namespaces.
-   *
-   * @return void
-   */
-  public function __construct()
-  { }
-
-  /**
-   * Sets the namespace separator used by classes in the namespace of this class loader.
-   *
-   * @param string $sep
-   * @return void
-   */
-  public function setNamespaceSeparator($separator)
-  {
-    $this->namespaceSeparator = $separator;
-  }
-
-  /**
-   * Gets the namespace separator used by classes in the namespace of this class loader.
-   *
-   * @return string $namespaceSeparator.
-   */
-  public function getNamespaceSeparator()
-  {
-    return $this->namespaceSeparator;
-  }
-
-  /**
-   * Sets the file extension of class files in the namespace of this class loader.
-   *
-   * @param string $fileExtension
-   * @return void
-   */
-  public function setFileExtension($fileExtension)
-  {
-    $this->fileExtension = $fileExtension;
-  }
-
-  /**
-   * Gets the file extension of class files in the namespace of this class loader.
-   *
-   * @return string $fileExtension
-   */
-  public function getFileExtension()
-  {
-    return $this->fileExtension;
-  }
-
-  /**
-   * Gets all currently registered namespaces with associated include paths
-   *
-   * @return array $namespaces
-   */
-  public function getRegisteredNamespaces()
-  {
-    return $this->namespaces;
-  }
-
-  /*
-   * Set the include path for a namespace and register the namespace in the class lookup
-   *
-   * @param string $ns
-   * @param string $includePath
-   * @return void
-   */
-  public function registerNamespace($namespace, $includePath = null)
-  {
-    $this->namespaces[$namespace] = $includePath;
-  }
-
-  /**
-   * Set the include path for multiple namespaces and register the namespace in the
-   * class lookup
-   *
-   * @param array $registrations
-   * @return void
-   */
-  public function registerNamespaces(array $registrations)
-  {
-    foreach($registrations as $namespace => $includePath)
-    {
-      $this->registerNamespace($namespace, $includePath);
-    }
-  }
-
-  /**
-   * Installs this class loader on the SPL autoload stack.
-   * Remove from autoloader stack with {@link unregister()}
-   *
-   * @see spl_autoload_register()
-   * @return void
-   */
-  public function register()
-  {
-    spl_autoload_register(array($this, 'loadClass'));
-  }
-
-  /**
-   * Uninstalls this class loader from the SPL autoloader stack.
-   *
-   * @see spl_autoload_unregister()
-   * @return void
-   */
-  public function unregister()
-  {
-    spl_autoload_unregister(array($this, 'loadClass'));
-  }
-
-  /**
-   * Loads the given class or interface.
-   *
-   * @param string $className
-   * @return void
-   */
-  public function loadClass($className)
-  {
-    //@fixme: bad loop for namespaces
-    foreach($this->namespaces as $namespace => $includePath)
-    {
-      if($namespace.$this->namespaceSeparator == substr($className, 0, strlen($namespace.$this->namespaceSeparator)))
-      {
-        if(false !== ($lastNsPos = strripos($className, $this->namespaceSeparator)))
-        {
-            $namespace = substr($className, 0, $lastNsPos);
-            $className = substr($className, $lastNsPos + 1);
-            $fileName = str_replace($this->namespaceSeparator, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-        }
-        
-        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . $this->fileExtension;
-
-        $path = ($includePath !== null ? $includePath . DIRECTORY_SEPARATOR : __DIR__ . DIRECTORY_SEPARATOR) . $fileName;
-
-        if(!file_exists($path))
-        {
-          throw new \RuntimeException(sprintf("File %s does not exist", $path));
-        }
-
-        require $path;
-
-        return true;
-      }
+    /**
+     * Creates a new Autoload instance that loads classes of the
+     * specified namespaces.
+     *
+     * @return void
+     */
+    public function __construct() {
     }
 
-    throw new \RuntimeException(sprintf("Class or Interface '%s' not found.", $className));
-  }
+    /**
+     * Sets the namespace separator used by classes in the namespace of this class loader.
+     *
+     * @param string $sep
+     * @return void
+     */
+    public function setNamespaceSeparator($separator) {
+        $this->namespaceSeparator = $separator;
+    }
+
+    /**
+     * Gets the namespace separator used by classes in the namespace of this class loader.
+     *
+     * @return string $namespaceSeparator.
+     */
+    public function getNamespaceSeparator() {
+        return $this->namespaceSeparator;
+    }
+
+    /**
+     * Sets the file extension of class files in the namespace of this class loader.
+     *
+     * @param string $fileExtension
+     * @return void
+     */
+    public function setFileExtension($fileExtension) {
+        $this->fileExtension = $fileExtension;
+    }
+
+    /**
+     * Gets the file extension of class files in the namespace of this class loader.
+     *
+     * @return string $fileExtension
+     */
+    public function getFileExtension() {
+        return $this->fileExtension;
+    }
+
+    /**
+     * Gets all currently registered namespaces with associated include paths
+     *
+     * @return array $namespaces
+     */
+    public function getRegisteredNamespaces() {
+        return $this->namespaces;
+    }
+
+    /*
+     * Set the include path for a namespace and register the namespace in the class lookup
+     *
+     * @param string $ns
+     * @param string $includePath
+     * @return void
+     */
+    public function registerNamespace($namespace, $includePath = null) {
+        $this->namespaces[$namespace] = $includePath;
+    }
+
+    /**
+     * Set the include path for multiple namespaces and register the namespace in the
+     * class lookup
+     *
+     * @param array $registrations
+     * @return void
+     */
+    public function registerNamespaces(array $registrations) {
+        foreach ($registrations as $namespace => $includePath) {
+          $this->registerNamespace($namespace, $includePath);
+        }
+    }
+
+    /**
+     * Installs this class loader on the SPL autoload stack.
+     * Remove from autoloader stack with {@link unregister()}
+     *
+     * @see spl_autoload_register()
+     * @return void
+     */
+    public function register() {
+        spl_autoload_register(array($this, 'loadClass'));
+    }
+
+    /**
+     * Uninstalls this class loader from the SPL autoloader stack.
+     *
+     * @see spl_autoload_unregister()
+     * @return void
+     */
+    public function unregister() {
+        spl_autoload_unregister(array($this, 'loadClass'));
+    }
+
+    /**
+     * Loads the given class or interface.
+     *
+     * @param string $className
+     * @return void
+     */
+    public function loadClass($className) {
+        //@fixme: bad loop for namespaces
+        foreach ($this->namespaces as $namespace => $includePath) {
+            if ($namespace.$this->namespaceSeparator == substr($className, 0, strlen($namespace.$this->namespaceSeparator))) {
+                if (false !== ($lastNsPos = strripos($className, $this->namespaceSeparator))) {
+                    $namespace = substr($className, 0, $lastNsPos);
+                    $className = substr($className, $lastNsPos + 1);
+                    $fileName = str_replace($this->namespaceSeparator, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+                }
+
+                $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . $this->fileExtension;
+
+                $path = ($includePath !== null ? $includePath . DIRECTORY_SEPARATOR : __DIR__ . DIRECTORY_SEPARATOR) . $fileName;
+
+                if (!file_exists($path)) {
+                    throw new \RuntimeException(sprintf("File %s does not exist", $path));
+                }
+
+                require $path;
+
+                return true;
+            }
+        }
+
+        throw new \RuntimeException(sprintf("Class or Interface '%s' not found.", $className));
+    }
 }
