a110698943f73d727755c00215f497987b1edfb0
Versionable <haswalt@me.com>
2010-12-01 11:01:59 +0000

    Moving all source and tests into Versionable namespace. Adjusting paths accordingly

---
+++ test/src/Versionable/Autoload/Mock/Mock.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://versionable.co.uk/license.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@versionable.co.uk so we can send you a copy immediately.
+ *
+ * Autoloader based on the SplClassLoader implementation that implements the
+ * technical interoperability standards for PHP 5.3 namespaces and class names.
+ *
+ * @category   Autoload
+ * @package    Autoload
+ * @subpackage UnitTests
+ * @link       Autoload
+ * @copyright  Copyright (c) 2010-2015 Versionable (http://www.versionable.co.uk)
+ * @license    http://versionable.co.uk/license.html New BSD License
+ *
+ * @author     Harry Walter <harry@versionable.co.uk>
+ *
+ * @filesource
+ */
+
+/**
+ * @namespace
+ */
+namespace Versionable\Autoload\Mock;
+
+/**
+ * @category   Autoload
+ * @package    Autoload
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2010-2015 Versionable (http://www.versionable.co.uk)
+ * @license    http://versionable.co.uk/license.html New BSD License
+ */
+class Mock
+{ }
